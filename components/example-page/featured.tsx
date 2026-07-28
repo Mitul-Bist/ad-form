60f2c4d69c4fa3c0b3c41990b135615fa1cb1de8
Mitul Bist <mitulbist@gmail.com>
2026-07-28 14:27:17 +0530

    tsx fixes

--- components/example-page/featured.tsx
+++ components/example-page/featured.tsx
@@ -1,9 +1,15 @@
-export default function Featured(props){
+interface featuredProps {
+    image : string,
+    title : string,
+    content : string
+}
+
+export default function Featured({image, title, content} : featuredProps){
     return(
         <div className="w-1/3 aspect-square py-4 flex flex-col justify-between items-center shadow-md shadow-black/30">
-            <img className="aspect-7/4 shrink-0 object-cover" src={props.image} alt="" />
-            <p className="text-xl">{props.title}</p>
-            <p className="text-black/70">{props.content}</p>
+            <img className="aspect-7/4 shrink-0 object-cover" src={image} alt="" />
+            <p className="text-xl">{title}</p>
+            <p className="text-black/70">{content}</p>
         </div>
     );
 }
--- components/example-page/newsElement.tsx
+++ components/example-page/newsElement.tsx
@@ -1,8 +1,13 @@
-export default function NewsElement(props){
+interface newsProps{
+    image : string,
+    title : string 
+}
+
+export default function NewsElement({image, title} : newsProps){
     return(
         <div className="w-full mt-1 aspect-4/3 py-4 flex flex-col justify-start items-center">
-            <img className="aspect-7/4 shrink-0 object-cover" src={props.image} alt="" />
-            <p className="mt-1">{props.title}</p>
+            <img className="aspect-7/4 shrink-0 object-cover" src={image} alt="" />
+            <p className="mt-1">{title}</p>
         </div>
     );
 }
